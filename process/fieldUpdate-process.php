93fcbc79e75eb63870a0ea1313973791aed246a4
Sanjiv Jaiswal <sanjivjaiswal04@gmail.com>
2023-01-09 09:13:51 +0545

    slight update in syntax for about

--- process/fieldUpdate-process.php
+++ process/fieldUpdate-process.php
@@ -3,7 +3,7 @@ include('..\connection.php');
 $user_id = $_GET['id'];
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $aboutText = $_POST['aboutField'];
-    $query = "UPDATE `users` SET `about`='$aboutText' WHERE id='$user_id'";
+    $query = "UPDATE users SET about='$aboutText' WHERE id='$user_id'";
     if (mysqli_query($conn, $query)) {
         header('Location: ..\index.php');
     } else {
